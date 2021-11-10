6caadad528aebd1ef5616fe9f727c3d3ec552101
annaovsy <popkovaanna101@gmail.com>
2021-11-10 14:20:39 +0300

    fix client

---
+++ MessengerClient/MessengerClient/ActionWithClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryUsers;
+
+namespace MessengerClient
+{
+    public class ActionWithClient
+    {
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        private BinaryFormatter bFormatter = new BinaryFormatter();
+        public TcpClient client { get; set; }
+        public NetworkStream stream { get; set; }
+
+        public ActionWithClient(TcpClient client, NetworkStream stream)
+        {
+            this.stream = stream;
+            this.client = client;
+        }
+
+        public User ClientConnection(User user)
+        {
+            if (!client.Connected)
+            {
+                client.Connect(host, port);
+            }
+            stream = client.GetStream();
+            bFormatter.Serialize(stream, user);
+            user = (User)this.bFormatter.Deserialize(stream);
+            return user;
+        }
+    }
+}
---
+++ MessengerClient/MessengerClient/SignIn.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibraryUsers;
+
+namespace MessengerClient
+{
+    public partial class SignIn : Form
+    {
+        public TcpClient client { get; set; }
+        public NetworkStream stream { get; set; }
+        public User user { get; set; }
+        ActionWithClient action;
+
+        public SignIn(TcpClient client, NetworkStream stream)
+        {
+            action = new ActionWithClient(client, stream);
+            this.stream = stream;
+            this.client = client;
+            InitializeComponent();
+        }
+
+        private void buttonIN_Click(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text == "" || textBoxPassword.Text == "")
+            {
+                MessageBox.Show("Все поля должны быть заполнены!");
+            }
+            else
+            {
+                user = new User
+                {
+                    Login = textBoxLogin.Text,
+                    Password = textBoxPassword.Text,
+                    Action = "SignIn"
+                };
+
+                try
+                {
+                    user = action.ClientConnection(user);
+                    this.stream = action.stream;
+                    DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Неверный Логин или Пароль!");
+                    this.Close();
+                }
+            }
+        }
+    }
+}
